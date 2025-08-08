8e89dddb63ddafccbc6e2e92477451b72e34e1d2
Kazem Firouzmandi <kf1375@hotmail.com>
2025-08-08 21:39:27 +0330

    Change TCS_LED_PIN configuration to PP

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -63,7 +63,7 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : TCS_LED_Pin */
   GPIO_InitStruct.Pin = TCS_LED_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(TCS_LED_GPIO_Port, &GPIO_InitStruct);
